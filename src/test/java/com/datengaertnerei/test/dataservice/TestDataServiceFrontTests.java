8b53bb70b126a43b8c9283230b5682991fe84829
Jens Dibbern <jens.dibbern@gmail.com>
2020-08-21 19:18:05 +0200

    add tests

---
+++ src/test/java/com/datengaertnerei/test/dataservice/TestDataServiceFrontTests.java
@@ -0,0 +1,30 @@
+package com.datengaertnerei.test.dataservice;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.ui.ExtendedModelMap;
+import org.springframework.ui.Model;
+
+@SpringBootTest
+public class TestDataServiceFrontTests {
+	
+	@Autowired
+	FrontEndController controller;
+	
+	@Test
+	void contextLoads() {
+		assertThat(controller).isNotNull();
+	}
+	
+	@Test
+	void shouldReturnRandomPersonModel(){
+		Model model = new ExtendedModelMap();
+		String view = controller.index(model);
+		assertThat(model.getAttribute("givenName")).isNotNull();
+		assertThat(view).isEqualTo("front");
+	}
+
+}
