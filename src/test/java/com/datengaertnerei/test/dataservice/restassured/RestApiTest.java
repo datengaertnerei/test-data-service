d023da2588ff86afc61c03f1940e01aa42ce606a
Jens Dibbern <jens.dibbern@gmail.com>
2021-04-05 18:31:12 +0200

    REST assured based test

---
+++ src/test/java/com/datengaertnerei/test/dataservice/restassured/RestApiTest.java
@@ -0,0 +1,33 @@
+package com.datengaertnerei.test.dataservice.restassured;
+
+import static io.restassured.RestAssured.get;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+
+/**
+ * REST assured based test
+ *
+ */
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+public class RestApiTest {
+
+	@Test
+	public void shouldReturnCreditCard() {
+		ValidatableResponse response = get("/api/v1/creditcard").then().assertThat().statusCode(200).
+		        and().contentType(ContentType.JSON).and().body("number", notNullValue()).body("type", notNullValue())
+				.body("cvc", notNullValue());
+		String expiry = response.extract().jsonPath().getString("expiry");
+		LocalDate localDate = LocalDate.parse(expiry, DateTimeFormatter.ISO_LOCAL_DATE);
+		assertThat(localDate).isAfter(LocalDate.now());
+	}
+}
--- src/test/resources/com/datengaertnerei/test/dataservice/cucumber/bankaccount.feature
+++ src/test/resources/com/datengaertnerei/test/dataservice/cucumber/bankaccount.feature
@@ -4,12 +4,8 @@ Feature: Verify the bank account api
     When the client asks for bank account in Hamburg
     Then the client receives status code OK
     And the client receives bank account in this city
-    
-  Scenario: client makes call to GET bank account
-    When the client asks for bank account in Berlin
-    Then the client receives status code OK
-    And the client receives bank account in this city
-    
+        
   Scenario: client makes call to GET bank account
     When the client asks for bank account in Unbekannt
     Then the client receives status code OK
+    
