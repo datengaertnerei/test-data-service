b30b4b0ac38d5173e1e2c2ea1c0ee998ab49418a
Jens Dibbern <jens.dibbern@gmail.com>
2021-11-20 10:08:56 +0100

    fix circular ref

--- src/main/java/com/datengaertnerei/test/dataservice/security/SecurityConfig.java
+++ src/main/java/com/datengaertnerei/test/dataservice/security/SecurityConfig.java
@@ -4,11 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
@@ -20,13 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private boolean enableBasicAuth;
 
 	@Autowired
-	private UserDetailsServiceImpl userDetailsService;
-
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		log.info("creating password encoder bean");
-		return new BCryptPasswordEncoder();
-	}
+	private UserDetailsService userDetailsService;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -42,7 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		log.info("setting up user auth details");
-		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+		auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
 
 	}
 
