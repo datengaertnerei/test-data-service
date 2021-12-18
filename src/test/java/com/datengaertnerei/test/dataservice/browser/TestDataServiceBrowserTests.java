d38f98ab356b42dba005b4bf75967ab793df874b
Jens Dibbern <jens.dibbern@gmail.com>
2021-12-18 01:13:04 +0100

    switch to derby db

--- src/test/java/com/datengaertnerei/test/dataservice/browser/TestDataServiceBrowserTests.java
+++ src/test/java/com/datengaertnerei/test/dataservice/browser/TestDataServiceBrowserTests.java
@@ -3,12 +3,16 @@ package com.datengaertnerei.test.dataservice.browser;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.web.server.LocalServerPort;
 
+import com.datengaertnerei.test.dataservice.person.DataImportTest;
+import com.datengaertnerei.test.dataservice.person.PostalAddressRepository;
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
@@ -19,6 +23,14 @@ class TestDataServiceBrowserTests {
 
 	@LocalServerPort
 	int randomServerPort;
+
+	@Autowired
+	private PostalAddressRepository repository;
+
+	@BeforeEach
+	public void before() {
+		DataImportTest.ensureDataAvailability(repository);
+	}
 
 	@Test
 	void testMe() {
