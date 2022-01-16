f27c0898ef7844b89324e83f2617523658570074
Jens Dibbern <44938643+datengaertnerei@users.noreply.github.com>
2022-01-16 18:40:03 +0100

    Delete src directory

--- src/main/java/com/datengaertnerei/test/dataservice/person/AgeRange.java
+++
@@ -1,5 +0,0 @@
-package com.datengaertnerei.test.dataservice.person;
-
-public enum AgeRange {
-	ALL, ADULT, SENIOR, MINOR 
-}
--- src/main/java/com/datengaertnerei/test/dataservice/security/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.datengaertnerei.test.dataservice.security;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<TestDataUser, String> {
-
-}
