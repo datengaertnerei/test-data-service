5dc9fde407c3bbd766cda01ea65410e9c57ca893
Jens Dibbern <jens.dibbern@gmail.com>
2022-02-13 00:38:20 +0100

    fix deprecated Cucumber setup

--- src/test/java/com/datengaertnerei/test/dataservice/cucumber/CucumberTest.java
+++ src/test/java/com/datengaertnerei/test/dataservice/cucumber/CucumberTest.java
@@ -1,10 +1,17 @@
 package com.datengaertnerei.test.dataservice.cucumber;
 
 import org.junit.jupiter.api.Tag;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-import io.cucumber.junit.platform.engine.Cucumber;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
-@Cucumber
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("com/datengaertnerei/test/dataservice/cucumber")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.datengaertnerei.test.dataservice.cucumber")
 @Tag("bdd")
 public class CucumberTest {
 	// empty class to start cucumber feature based tests
