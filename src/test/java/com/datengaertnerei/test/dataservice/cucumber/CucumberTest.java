8010f0fb8425fae0435c10e6fbd5dd6fa8611cfb
Jens Dibbern <44938643+datengaertnerei@users.noreply.github.com>
2022-06-21 19:05:03 +0200

    cleanup + V1

--- src/test/java/com/datengaertnerei/test/dataservice/cucumber/CucumberTest.java
+++ src/test/java/com/datengaertnerei/test/dataservice/cucumber/CucumberTest.java
@@ -1,12 +1,12 @@
 package com.datengaertnerei.test.dataservice.cucumber;
 
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
 import org.junit.jupiter.api.Tag;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
-
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
